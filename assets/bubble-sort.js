1001c96dd005a41aca852da019b6bf081768b50b
Mohit-Davera <80089514+Mohit-Davera@users.noreply.github.com>
2021-06-15 19:01:04 +0530

    Add files via upload

--- assets/bubble-sort.js
+++ assets/bubble-sort.js
@@ -1,8 +1,10 @@
 async function bubbleSort() {
     var i, j;
+    var swapped;
     await sleep(delay);
 
-    for(i = 0; i < size - 1; i++) {
+    for (i = 0; i < size - 1; i++) {
+        swapped = false;
         for(j = 0; j < size - i - 1; j++) {
             await sleep(delay);
 
@@ -10,13 +12,17 @@ async function bubbleSort() {
             setColor(j + 1, COMPARE);
             await sleep(delay);
 
-            if(arr[j] > arr[j + 1]) {
+            if (arr[j] > arr[j + 1]) {
+                swapped = true;
                 swap(j, j + 1);
                 await sleep(delay);
             }
-
             setColor(j, UNSORTED);
             setColor(j + 1, UNSORTED);
+        }
+        if (!swapped) {
+            setColorRange(0, arr.length, SORTED);
+            break
         }
 
         await sleep(delay);
